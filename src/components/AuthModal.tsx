d73d4490afd05f3ff6d779b8f631beb1f74b9d15
alexjoshva <alexchandarjoshva@gmail.com>
2026-05-03 15:43:16 +0530

    feat: implement global CSS styling and AuthModal component for user authentication

--- src/components/AuthModal.tsx
+++ src/components/AuthModal.tsx
@@ -120,11 +120,14 @@ export default function AuthModal({ isOpen, onClose }: AuthModalProps) {
   useEffect(() => {
     if (isOpen) {
       document.body.classList.add('modal-open')
+      document.documentElement.classList.add('modal-open')
     } else {
       document.body.classList.remove('modal-open')
+      document.documentElement.classList.remove('modal-open')
     }
     return () => {
       document.body.classList.remove('modal-open')
+      document.documentElement.classList.remove('modal-open')
     }
   }, [isOpen])
 
